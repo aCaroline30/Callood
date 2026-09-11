2bb224d7e9cd07d7bbcf4b2435d5ec7516dda043
Rômulo Alves <romulo.gamedesigner@gmail.com>
2026-09-11 12:54:31 -0300

    Address CRUD working

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Address extends Model
 {
+    protected $table = "address";
+
     protected $fillable = [
         'street',
         'number',
